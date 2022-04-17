b019999b1e0cd65ca369a2413082039422aee2e8
Henri <henrimaronen@gmail.com>
2022-04-17 20:46:51 +0300

    author file, header admin panel

--- header.php
+++ header.php
@@ -9,20 +9,21 @@
 		}
 		return $count;
 	}
-
 ?>
 
 <ul>
 	<li><a class="active" href="index.php">Home</a></li>
 	<li><a href="#product">Products</a></li>
 	<li><a href="basket.php">Basket ( <?= product_count($_SESSION['cart']); ?> )</a></li>
-	<!-- If statement if we are logged in to show log out button? -->
 	<div style="float: right">
-	<?PHP
+	<?php
+	if($_SESSION["username"] === "admin") { ?>
+		<li><a href="admin.php">Admin Panel</a></li> <?php } ?>
+		<?PHP
 		if($_SESSION["username"]) { ?>
 			<li><a>Hello, <?php echo $_SESSION["username"]; ?></a></li>
 			<li><a href="logout.php">Log Out</a></li>
-		<?PHP }
+			<?PHP }
 		else { ?>
 		<li><a href="login.php">Sign Up</a></li>
 		<?PHP } ?>
