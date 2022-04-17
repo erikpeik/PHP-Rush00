55d1d1627c42a59c7992ca13761d1746cd5b0ce1
Erik Mende <emende@student.hive.fi>
2022-04-17 23:27:44 +0300

    New nav bar

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <?PHP
 	session_start();
+	require_once("connect_db.php");
 	function	product_count($basket)
 	{
 		$count = 0;
@@ -9,23 +10,44 @@
 		}
 		return $count;
 	}
+
+	function	total_price()
+	{
+		$sql = "SELECT * FROM products";
+		$result = mysqli_query($con, $sql);
+		$total = 0;
+		while ($row = mysqli_fetch_assoc($result))
+		{
+			foreach ($_SESSION["cart"] as $product)
+			{
+				if ($row['id'] == $product['product_id'])
+					$total += $row['price'] * $product["count"];
+			}
+		}
+	}
 ?>
 
-<ul>
-	<li><a class="active" href="index.php">Home</a></li>
-	<li><a href="#product">Products</a></li>
-	<li><a href="basket.php">Basket ( <?= product_count($_SESSION['cart']); ?> )</a></li>
+<div class="navbar">
+	<a class="active" href="index.php">Home</a>
+	<div class="dropdown">
+		<button class="dropbtn">Products</button>
+		<div class="dropdown-content">
+			<a href="#">Link 1</a>
+			<a href="#">Link 2</a>
+		</div>
+	</div>
+	<a class="active" href="basket.php">Basket ( <?= product_count($_SESSION['cart']) ?> )</a>
 	<div style="float: right">
 	<?php
-	if($_SESSION["username"] === "admin") { ?>
-		<li><a href="admin.php">Admin Panel</a></li> <?php } ?>
-		<?PHP
-		if($_SESSION["username"]) { ?>
-			<li><a href="user_settings.php">Hello, <?php echo $_SESSION["username"]; ?></a></li>
-			<li><a href="logout.php">Log Out</a></li>
-			<?PHP }
-		else { ?>
-		<li><a href="login.php">Log In</a></li>
+		if($_SESSION["username"] === "admin") { ?>
+			<a class="active" href="admin.php">Admin Panel</a> <?php } ?>
+			<?PHP
+			if($_SESSION["username"]) { ?>
+				<a class="active" href="user_settings.php">Hello, <?php echo $_SESSION["username"]; ?></a>
+				<a class="active" href="logout.php">Log Out</a>
+				<?PHP }
+			else { ?>
+			<a class="active" href="login.php">Log In</a>
 		<?PHP } ?>
 	</div>
-</ul>
+</div>
